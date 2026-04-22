3c910638f35f18b7cd0faf6bd688c98aee0b0657
Fuad <alasbahi.fuad@gmail.com>
2026-04-22 21:21:28 +0300

    Fix go_router navigation in evaluation summary screen

--- lib/features/evaluation/screens/evaluation_summary_report_screen.dart
+++ lib/features/evaluation/screens/evaluation_summary_report_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import '../../../core/theme/app_colors.dart';
 import '../../../core/theme/app_typography.dart';
 import '../../../shared/widgets/app_card.dart';
